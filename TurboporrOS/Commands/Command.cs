bdf19cf662034d22dc5f13985019a491d78c8276
Jordi Dragan <121635607+Turboporro69@users.noreply.github.com>
2024-12-22 00:11:30 +0100

    Agregar archivos de proyecto.

---
+++ TurboporrOS/Commands/Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurboporrOS.Commands
+{
+    public class Command
+    {
+        public readonly string name;
+        public Command(String name) { this.name = name; }
+    }
+}
---
+++ TurboporrOS/Kernel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace TurboporrOS
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Welcome to TurboporrOS, an operating system made with C# just for fun");
+        }
+
+        protected override void Run()
+        {
+
+        }
+    }
+}
